8b42d3d4a09de17439fb3fcf0bbff59ce9a3aeb7
Danjame <40506250+Danjame@users.noreply.github.com>
2019-06-28 20:56:29 +0100

    update

--- JS/mobile/animationMobile.js
+++ JS/mobile/animationMobile.js
@@ -129,7 +129,7 @@ function timeLineAnimationMobile() {
     (function timeLineAni() {
         cancelAnimationFrame(timeLineTimer);
         timeLineTimer = requestAnimationFrame(() => {
-            lineHeight += 1;
+            lineHeight += 0.5;
             if (lineHeight <= 239) {
                 timeLine.style.height = `${lineHeight}px`;
                 timeLineAni();
