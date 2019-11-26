7eb25fdd0f4f7ceefa04205a85a3f5f7b1a9a5d8
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2019-11-26 22:22:22 +0000

    finishing animation.js (rewrite)

--- JS/animation.js
+++ JS/animation.js
@@ -4,7 +4,7 @@ const controller = (() => {
         eachImg: "#imgGroup>li:first-child img",
         container: "#container",
         bgBox: "#bgBox",
-        imgNum:"#imgGroup>li",
+        imgNum: "#imgGroup>li",
     };
 
     return {
@@ -53,15 +53,15 @@ controller.forDeskTop();
 //适配Ipad
 // if (window.matchMedia("(width:768px)").matches & window.matchMedia("(height:1024px)").matches) {
 //     forTablet();
-//     getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
-//         if (imgGroup.style.transition === "all 1s ease 0s") {
-//             return;
-//         } else {
-//             skateAnimation(320, 9600, 50);
-//             bgAnimationTablet(2, 1);
-//             timeAnimation(123, 243, 363, 437, 25);
-//         }
-//     }, false);
+    // getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
+    //     if (imgGroup.style.transition === "all 1s ease 0s") {
+    //         return;
+    //     } else {
+    //         obj.skateAnimation(320, 9600, 50);
+    //          obj.bgAnimationTablet(2, 1);
+    //          obj.timeAnimation(123, 243, 363, 437, 25);
+    //     }
+    // }, false);
 //     //适配Ipad Pro
 // } else if (window.matchMedia("(width:1024px)").matches & window.matchMedia("(height:1366px)").matches) {
 //     forTablet();
@@ -75,31 +75,31 @@ controller.forDeskTop();
 //         }
 //     }, false);
 // } else { //桌面浏览器
-//     forDeskTop();
-//     getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
-//         if (imgGroup.style.transition === "all 1s ease 0s") {
-//             return;
-//         } else {
-//             skateAnimation(320, 9600, 50);
-//             bgAnimation(1, 1);
-//             if (window.matchMedia("(min-width:1301px)").matches) {
-//                 timeAnimation(180, 357, 532, 650, 25);
-//             } else if (window.matchMedia("(min-width:1131px)").matches) {
-//                 timeAnimation(143, 278, 413, 495, 27);
-//             } else if (window.matchMedia("(max-width:1130px)").matches) {
-//                 timeAnimation(113, 218, 323, 385, 30);
-//             }
-//         }
-//     }, false);
+    // forDeskTop();
+    getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
+        if (imgGroup.style.transition === "all 1s ease 0s") {
+            return;
+        } else {
+            obj.skateAnimation();
+            obj.bgAnimation();
+            if (window.matchMedia("(min-width:1301px)").matches) {
+                obj.timeAnimation(180, 357, 532, 650, 25);
+            } else if (window.matchMedia("(min-width:1131px)").matches) {
+                obj.timeAnimation(143, 278, 413, 495, 27);
+            } else if (window.matchMedia("(max-width:1130px)").matches) {
+                obj.timeAnimation(113, 218, 323, 385, 30);
+            }
+        }
+    }, false);
 // };
-// //监听点击事件, 停止动画
-// getEle("#btnBox input:nth-child(3)").addEventListener("click", () => {
-//     clearAllTimer();
-// }, false);
-// //绑定重看事件
-// getEle("#btnBox input:nth-child(4)").addEventListener("click", () => {
-//     initialization();
-// }, false);
+//监听点击事件, 停止动画
+getEle("#btnBox input:nth-child(3)").addEventListener("click", () => {
+    obj.clearAllTimer();
+}, false);
+//绑定重看事件
+getEle("#btnBox input:nth-child(4)").addEventListener("click", () => {
+    obj.initialization();
+}, false);
 
 
 
@@ -136,12 +136,14 @@ function Animation(obj) {
     this.totalFrame = 9600;
     this.skateMan = "#mainLeft";
     this.skateTimer = null;
-    // this.timeLine = obj.timeLine;
+
+    this.lineWidth = 0;
+    this.timeLine = "#timeLine" || obj.timeLine;
     // this.lineHeight = obj.lineHeight || 0;
-    // this.timeDots = obj.timeDots;
-    // this.cvWraps = obj.cvWraps;
-    // this.timeLineTimer = null;
-    // this.contentTimer = null;
+    this.timeDots = "#timeDot>li";
+    this.cvWraps = ".cvInfo";
+    this.timeLineTimer = null;
+    this.contentTimer = null;
 }
 
 //背景动画函数
@@ -155,7 +157,6 @@ Animation.prototype.bgAnimation = function() {
             this.bgOffset -= 1;
             if (this.bgOffset < -imgGroup.offsetWidth + eachImg.offsetWidth) {
                 clearTimeout(this.bgTimer);
-                console.log("end")
             } else {
                 imgGroup.style.transform = `translate3d(${this.bgOffset}px, 0, 0)`;
                 bgAni();
@@ -165,7 +166,7 @@ Animation.prototype.bgAnimation = function() {
     bgAni();
 };
 
-obj.bgAnimation();
+// obj.bgAnimation();
 //背景动画函数（平板适配）
 // function bgAnimationTablet(distance, ms) {
 //     clearTimeout(bgTimer);
@@ -195,56 +196,70 @@ Animation.prototype.skateAnimation = function() {
 }
 // obj.skateAnimation();
 //时间动画函数
-function timeAnimation(firstWidth, secondWidth, thirdWidth, totalWidth, timeLineMS) {
-    let lineWidth = timeLine.offsetWidth;;
-    (function lineAnimation() {
-        timeLineTimer = setTimeout(() => {
-            lineWidth += 1;
-            if (lineWidth <= totalWidth) {
-                timeLine.style.width = `${lineWidth}px`;
+
+Animation.prototype.timeAnimation = function(firstWidth, secondWidth, thirdWidth, totalWidth, timeLineMS) {
+    // let lineWidth = timeLine.offsetWidth;
+    const timeLine = document.querySelector(this.timeLine);
+    const lineAnimation = () => {
+        this.timeLineTimer = setTimeout(() => {
+            this.lineWidth += 1;
+            if (this.lineWidth <= totalWidth) {
+                timeLine.style.width = `${this.lineWidth}px`;
                 lineAnimation();
             } else {
-                clearTimeout(timeLineTimer);
+                clearTimeout(this.timeLineTimer);
             }
         }, timeLineMS);
-    })();
-    //文本框和时间点根据时间轴长度分别淡出和显示
-    (function contentAnimation() {
-        displayDotTest(dots[0], cvWraps[0]);
+    };
+    lineAnimation();
+    // 文本框和时间点根据时间轴长度分别淡出和显示
 
-        contentTimer = setInterval(() => {
-            lineWidth = timeLine.offsetWidth;
-            switch (lineWidth) {
+    const dots = document.querySelectorAll(this.timeDots);
+    const cvWraps = document.querySelectorAll(this.cvWraps);
+console.log(dots);
+console.log(cvWraps);
+    const displayDotText = (dot, textWrap) => {
+        dot.style.display = "block";
+        textWrap.className = "cvWrapFrames";
+    }
+
+    const self = this;
+
+    (function contentAnimation() {
+        displayDotText(dots[0], cvWraps[0]);
+        self.contentTimer = setInterval(() => {
+            
+            switch (self.lineWidth) {
                 case firstWidth:
-                    displayDotTest(dots[2], cvWraps[2]);
+                    displayDotText(dots[2], cvWraps[2]);
                     break;
                 case secondWidth:
-                    displayDotTest(dots[1], cvWraps[1]);
+                    displayDotText(dots[1], cvWraps[1]);
                     break;
                 case thirdWidth:
-                    displayDotTest(dots[3], cvWraps[3]);
+                    displayDotText(dots[3], cvWraps[3]);
                     break;
                 case totalWidth:
-                    clearInterval(contentTimer);
+                    clearInterval(self.contentTimer);
                     break;
             }
         }, 10);
-
-        function displayDotTest(dot, textWrap) {
-            dot.style.display = "block";
-            textWrap.className = "cvWrapFrames";
-        }
     })();
 }
+// obj.timeAnimation(180, 357, 532, 650, 25);
 //还原函数
-function initialization() {
-    clearAllTimer();
-    offset = 0;
-    imgGroup.style.transform = `translate3d(${offset}px, 0, 0)`;
+Animation.prototype.initialization = function() {
+    const imgGroup = document.querySelector(this.imgGroup);
+    const dots = document.querySelectorAll(this.timeDots);
+    const cvWraps = document.querySelectorAll(this.cvWraps);
+    const timeLine = document.querySelector(this.timeLine);
+
+    this.clearAllTimer();
+    this.bgOffset = 0;
+    imgGroup.style.transform = `translate3d(${this.bgOffset}px, 0, 0)`;
     imgGroup.style.transition = "all 1s ease 0s";
-    imgGroup.addEventListener("transitionend", () => {
-        imgGroup.style.transition = "all 0s linear 0s";
-    }, false)
+
+
 
     dots.forEach(item => {
         item.style.display = "none";
@@ -254,12 +269,21 @@ function initialization() {
         item.className = "";
     })
 
-    timeLine.style.width = 0;
+    this.lineWidth = 0;
+    timeLine.style.width = this.lineWidth;
+
+
+    const transitionEnd = () => {
+        imgGroup.style.transition = "all 0s linear 0s";
+        imgGroup.removeEventListener("transitionend", transitionEnd);
+    }
+
+    imgGroup.addEventListener("transitionend", transitionEnd, false)
 }
 
-function clearAllTimer() {
-    clearTimeout(skateTimer);
-    clearTimeout(bgTimer);
-    clearTimeout(timeLineTimer);
-    clearInterval(contentTimer);
+Animation.prototype.clearAllTimer = function() {
+    clearTimeout(this.skateTimer);
+    clearTimeout(this.bgTimer);
+    clearTimeout(this.timeLineTimer);
+    clearInterval(this.contentTimer);
 }
