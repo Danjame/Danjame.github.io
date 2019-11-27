b15328af0e3c291bb82f2fb1be46fada252d242d
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2019-11-27 19:11:39 +0000

    rewriting commonMobile.js finished

--- JS/animation.js
+++ JS/animation.js
@@ -1,108 +1,3 @@
-const Setting = (() => {
-    const elements = {
-        imgGroup: "#imgGroup",
-        eachImg: "#imgGroup>li:first-child img",
-        container: "#container",
-        bgBox: "#bgBox",
-        imgNum: "#imgGroup>li",
-    };
-
-    return {
-        getEle(ele) {
-            return document.querySelector(ele);
-        },
-        getEles(eles) {
-            return document.querySelectorAll(eles);
-        },
-        //桌面浏览器自动适配
-        forDeskTop() {
-            const container = this.getEle("#container");
-            const bgBox = this.getEle("#bgBox");
-            const imgGroup = this.getEle("#imgGroup");
-            const eachImg = this.getEle("#imgGroup>li:first-child img");
-            const imgNum = this.getEles("#imgGroup>li").length;
-
-            container.style.width = bgBox.style.width = `${eachImg.offsetWidth}px`;
-            imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
-
-            window.addEventListener("resize", () => {
-                imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
-                container.style.width = bgBox.style.width = `${eachImg.offsetWidth}px`;
-            }, false)
-        },
-        //平板自动适配
-        forTablet() {
-            const container = this.getEle("#container");
-            const bgBox = this.getEle("#bgBox");
-            const imgGroup = this.getEle("#imgGroup");
-            const eachImg = this.getEle("#imgGroup>li:first-child img");
-            const imgNum = this.getEles("#imgGroup>li").length;
-            const screenWidth = document.documentElement.clientWidth;
-
-            bgBox.style.height = `${eachImg.offsetHeight}px`;
-            imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
-            container.style.width = bgBox.style.width = `${screenWidth}px`;
-        },
-        screenSetting() {
-            if ((window.matchMedia("(width:768px)").matches && window.matchMedia("(height:1024px)").matches) ||
-                (window.matchMedia("(width:1024px)").matches && window.matchMedia("(height:1366px)").matches)) {
-                this.forTablet();
-            } else {
-                this.forDeskTop();
-            };
-        },
-        ipadSetting() {
-            aboutMe.skateAnimation();
-            aboutMe.bgAnimationTablet();
-            aboutMe.timeAnimation(123, 243, 363, 437, 25);
-        },
-        ipadProSetting() {
-            aboutMe.skateAnimation();
-            aboutMe.bgAnimationTablet();
-            aboutMe.timeAnimation(166, 330, 496, 596, 25);
-        },
-        deskTopSetting() {
-            aboutMe.skateAnimation();
-            aboutMe.bgAnimation();
-            if (window.matchMedia("(min-width:1301px)").matches) {
-                aboutMe.timeAnimation(180, 357, 532, 650, 25);
-            } else if (window.matchMedia("(min-width:1131px)").matches) {
-                aboutMe.timeAnimation(143, 278, 413, 495, 27);
-            } else if (window.matchMedia("(max-width:1130px)").matches) {
-                aboutMe.timeAnimation(113, 218, 323, 385, 30);
-            }
-        }
-    }
-})();
-
-const SetListner = ((Setting) => {
-    return {
-        init() {
-            console.log("The animation is ready!");
-            // 适配Ipad
-            Setting.getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
-                if (imgGroup.style.transitionDuration !== "1s") {
-                    if (window.matchMedia("(width:768px)").matches && window.matchMedia("(height:1024px)").matches) {
-                        Setting.ipadSetting()
-                    } else if (window.matchMedia("(width:1024px)").matches && window.matchMedia("(height:1366px)").matches) {
-                        Setting.ipadProSetting()
-                    } else {
-                        Setting.deskTopSetting()
-                    }
-                }
-            }, false);
-            //监听点击事件, 停止动画
-            Setting.getEle("#btnBox input:nth-child(3)").addEventListener("click", () => {
-                aboutMe.clearAllTimer();
-            }, false);
-            //绑定重看事件
-            Setting.getEle("#btnBox input:nth-child(4)").addEventListener("click", () => {
-                aboutMe.initialization();
-            }, false)
-        }
-    }
-})(Setting);
-
 const aboutMe = new Animation({
     imgGroup: "#imgGroup",
     eachImg: "#imgGroup>li:first-child img",
@@ -236,7 +131,7 @@ Animation.prototype.timeAnimation = function(firstWidth, secondWidth, thirdWidth
         }, 10);
     })();
 }
-//初始化
+//初始化动画
 Animation.prototype.initialization = function() {
     const imgGroup = document.querySelector(this.imgGroup);
     const dots = document.querySelectorAll(this.timeDots);
@@ -273,6 +168,111 @@ Animation.prototype.clearAllTimer = function() {
     clearTimeout(this.timeLineTimer);
     clearInterval(this.contentTimer);
 }
+
+const Setting = (() => {
+    const elements = {
+        imgGroup: "#imgGroup",
+        eachImg: "#imgGroup>li:first-child img",
+        container: "#container",
+        bgBox: "#bgBox",
+        imgNum: "#imgGroup>li",
+    };
+
+    return {
+        getEle(ele) {
+            return document.querySelector(ele);
+        },
+        getEles(eles) {
+            return document.querySelectorAll(eles);
+        },
+        //桌面浏览器自动适配
+        forDeskTop() {
+            const container = this.getEle("#container");
+            const bgBox = this.getEle("#bgBox");
+            const imgGroup = this.getEle("#imgGroup");
+            const eachImg = this.getEle("#imgGroup>li:first-child img");
+            const imgNum = this.getEles("#imgGroup>li").length;
+
+            container.style.width = bgBox.style.width = `${eachImg.offsetWidth}px`;
+            imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
+
+            window.addEventListener("resize", () => {
+                imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
+                container.style.width = bgBox.style.width = `${eachImg.offsetWidth}px`;
+            }, false)
+        },
+        //平板自动适配
+        forTablet() {
+            const container = this.getEle("#container");
+            const bgBox = this.getEle("#bgBox");
+            const imgGroup = this.getEle("#imgGroup");
+            const eachImg = this.getEle("#imgGroup>li:first-child img");
+            const imgNum = this.getEles("#imgGroup>li").length;
+            const screenWidth = document.documentElement.clientWidth;
+
+            bgBox.style.height = `${eachImg.offsetHeight}px`;
+            imgGroup.style.width = `${eachImg.offsetWidth * imgNum}px`;
+            container.style.width = bgBox.style.width = `${screenWidth}px`;
+        },
+        screenSetting() {
+            if ((window.matchMedia("(width:768px)").matches && window.matchMedia("(height:1024px)").matches) ||
+                (window.matchMedia("(width:1024px)").matches && window.matchMedia("(height:1366px)").matches)) {
+                this.forTablet();
+            } else {
+                this.forDeskTop();
+            };
+        },
+        ipadSetting() {
+            aboutMe.skateAnimation();
+            aboutMe.bgAnimationTablet();
+            aboutMe.timeAnimation(123, 243, 363, 437, 25);
+        },
+        ipadProSetting() {
+            aboutMe.skateAnimation();
+            aboutMe.bgAnimationTablet();
+            aboutMe.timeAnimation(166, 330, 496, 596, 25);
+        },
+        deskTopSetting() {
+            aboutMe.skateAnimation();
+            aboutMe.bgAnimation();
+            if (window.matchMedia("(min-width:1301px)").matches) {
+                aboutMe.timeAnimation(180, 357, 532, 650, 25);
+            } else if (window.matchMedia("(min-width:1131px)").matches) {
+                aboutMe.timeAnimation(143, 278, 413, 495, 27);
+            } else if (window.matchMedia("(max-width:1130px)").matches) {
+                aboutMe.timeAnimation(113, 218, 323, 385, 30);
+            }
+        }
+    }
+})();
+
+const SetListner = ((Setting) => {
+    return {
+        init() {
+            console.log("The animation is ready!");
+            // 适配Ipad
+            Setting.getEle("#btnBox input:nth-child(2)").addEventListener("click", () => {
+                if (imgGroup.style.transitionDuration !== "1s") {
+                    if (window.matchMedia("(width:768px)").matches && window.matchMedia("(height:1024px)").matches) {
+                        Setting.ipadSetting()
+                    } else if (window.matchMedia("(width:1024px)").matches && window.matchMedia("(height:1366px)").matches) {
+                        Setting.ipadProSetting()
+                    } else {
+                        Setting.deskTopSetting()
+                    }
+                }
+            }, false);
+            //监听点击事件, 停止动画
+            Setting.getEle("#btnBox input:nth-child(3)").addEventListener("click", () => {
+                aboutMe.clearAllTimer();
+            }, false);
+            //绑定重看事件
+            Setting.getEle("#btnBox input:nth-child(4)").addEventListener("click", () => {
+                aboutMe.initialization();
+            }, false)
+        }
+    }
+})(Setting);
 
 Setting.screenSetting();
 SetListner.init();
--- JS/mobile/commonMobile.js
+++ JS/mobile/commonMobile.js
@@ -1,20 +1,34 @@
-// 头部菜单
-const menu = getEle("#header #menu");
-const menuList = getEle("#header div:last-child div:first-child");
-const menuBars = getAll("#header #menu>span");
-//头部菜单动画
-menu.addEventListener("click", () => {
-    if (menuList.className === "activedMenu") {
-        for (let i = 0; i < menuBars.length; i++) {
-            menuBars[i].className = "menuBarsOff";
-        };
-        menuList.className = "unactivedMenu";
-        menu.className = "unclickMenu";
-    } else {
-        for (let i = 0; i < menuBars.length; i++) {
-            menuBars[i].className = "menuBarsOn";
-        };
-        menuList.className = "activedMenu";
-        menu.className = "clickMenu";
+const MenuEvents = (() => {
+    const elements = {
+        menu: "#menu",
+        menuList: "#header div:last-child div:first-child",
+        menuBars: "#menu>span"
     }
-}, false);
+
+    return {
+        getEle(ele) {
+            return document.querySelector(ele);
+        },
+        getEles(eles) {
+            return document.querySelectorAll(eles);
+        },
+        menuListener() {
+            // 头部菜单
+            this.getEle(elements.menu).addEventListener("click", () => {
+                const menuList = this.getEle(elements.menuList);
+                const menuBars = this.getEles(elements.menuBars);
+                if (menuList.className === "activedMenu") {
+                    menuBars.forEach(item=>item.className = "menuBarsOff");
+                    menuList.className = "unactivedMenu";
+                    menu.className = "unclickMenu";
+                } else {
+                    menuBars.forEach(item=>item.className = "menuBarsOn");
+                    menuList.className = "activedMenu";
+                    menu.className = "clickMenu";
+                }
+            }, false)
+        }
+    }
+})();
+
+MenuEvents.menuListener();
